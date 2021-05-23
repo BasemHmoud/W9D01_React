ae697ee279e8f719dbdad3aa386874e15ca7acf3
C05_Basem <basemhmoud735@gmail.com>
2021-05-24 00:45:42 +0300

    finish pulse check from 1 to 6

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const todos=[{todo: "wake up", id: 1}, {todo: "breakfast", id: 2}, {todo: "study", id: 3}]
+// jsx
+const App = () => {
+  return <div > <h1> Todo List </h1></div >
+};
+
+export default App
